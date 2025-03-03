bd239d4c7d1d363bd7f35604c7704e9ed8e1ff99
Greg Meyer <greg.meyer@broadcom.com>
2025-03-03 08:17:18 -0600

    Cleaning up some autoconfiguration in restful-agent-app.

--- restful-agent-app/src/main/java/com/example/memgptagent/MemgptAgentApplication.java
+++ restful-agent-app/src/main/java/com/example/memgptagent/MemgptAgentApplication.java
@@ -1,14 +1,12 @@
 package com.example.memgptagent;
 
 
-import org.springframework.ai.autoconfigure.openai.OpenAiAutoConfiguration;
 import org.springframework.ai.autoconfigure.vectorstore.pgvector.PgVectorStoreAutoConfiguration;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
-@SpringBootApplication(exclude = {
-		OpenAiAutoConfiguration.class, PgVectorStoreAutoConfiguration.class})
+@SpringBootApplication(exclude = {PgVectorStoreAutoConfiguration.class})
 public class MemgptAgentApplication {
 
 	public static void main(String[] args) {
