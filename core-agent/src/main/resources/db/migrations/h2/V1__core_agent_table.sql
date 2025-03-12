b1f5aadde1a97dd7f0ef4aa3b965d6aeb29a0bc3
Greg Meyer <greg.meyer@broadcom.com>
2025-03-12 08:22:46 -0500

    Adding flywaydb and postgres support as well as cfenv for CF bindings.
    - Supports schema management and migration via flywaydb
    - Added postgres SQL files for postgres support
    - Updated to Spring Boot 3.4.3
    - Added cfenv to rest and mcp server for CF service binding to postgres

---
+++ core-agent/src/main/resources/db/migrations/h2/V1__core_agent_table.sql
@@ -0,0 +1,59 @@
+CREATE
+    TABLE
+    agents(
+              id uuid NOT NULL PRIMARY KEY,
+              version INTEGER NOT NULL,
+              create_dt_time TIMESTAMP WITH TIME ZONE NOT NULL,
+              update_dt_time TIMESTAMP WITH TIME ZONE NOT NULL,
+              agent_name VARCHAR(100) NOT NULL,
+              description VARCHAR(500) NOT NULL DEFAULT '',
+              message_ids json NOT NULL,
+              tool_ids json NOT NULL,
+              system_prompt VARCHAR(50000) NOT NULL,
+              context_window INTEGER NOT NULL,
+              metadata_labels json
+);
+
+CREATE
+    TABLE
+    messages(
+                id uuid NOT NULL PRIMARY KEY,
+                agent_id uuid NOT NULL,
+                role VARCHAR(20) NOT NULL,
+                text VARCHAR,
+                model VARCHAR(50),
+                name VARCHAR(100),
+                tool_calls json NOT NULL,
+                tool_call_id VARCHAR,
+                create_dt_time TIMESTAMP WITH TIME ZONE NOT NULL,
+                update_dt_time TIMESTAMP WITH TIME ZONE NOT NULL,
+                step_id VARCHAR(100)
+);
+
+CREATE
+    TABLE
+    blocks(
+              id uuid NOT NULL PRIMARY KEY,
+              version INTEGER NOT NULL,
+              agent_id uuid NOT NULL,
+              block_value VARCHAR NOT NULL,
+              size_limit INTEGER NOT NULL,
+              label VARCHAR(50) NOT NULL,
+              metadata json,
+              description VARCHAR(500) NOT NULL DEFAULT '',
+              create_dt_time TIMESTAMP WITH TIME ZONE NOT NULL,
+              update_dt_time TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+CREATE
+    TABLE
+    tools(
+             id uuid NOT NULL PRIMARY KEY,
+             name VARCHAR(100) NOT NULL,
+             description VARCHAR(500) DEFAULT '',
+             fq_class_name VARCHAR(256) NOT NULL,
+             fq_input_class_name VARCHAR(256),
+             return_character_limit INTEGER,
+             create_dt_time TIMESTAMP WITH TIME ZONE NOT NULL,
+             update_dt_time TIMESTAMP WITH TIME ZONE NOT NULL
+);
--- core-agent/src/test/java/com/example/memgptagent/TestCoreAgentApplication.java
+++ core-agent/src/test/java/com/example/memgptagent/TestCoreAgentApplication.java
@@ -4,7 +4,11 @@ import org.springframework.ai.autoconfigure.openai.OpenAiAutoConfiguration;
 import org.springframework.ai.autoconfigure.vectorstore.pgvector.PgVectorStoreAutoConfiguration;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 import static org.mockito.Mockito.mock;
 
@@ -16,4 +20,12 @@ public class TestCoreAgentApplication {
     ChatClient chatClient() {
         return mock(ChatClient.class);
     }
+
+    @Profile("postgresql")
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    }
+
 }
